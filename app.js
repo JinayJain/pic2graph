a7bdcc1e6fcacb2b36b1b42ea9b8811f86cae192
Jinay Jain <jinaybjain@gmail.com>
2019-03-18 20:42:44 -0400

    I fixed lots of stuff and optimized the code significantly

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var pixels;
 
-let thresh = 50;
+let thresh = 10;
 
 function processImg(){
 
     var uploader = document.querySelector('input[type=file]').files[0];
-    var canvas = document.createElement("canvas");
+    var canvas = document.getElementById("cnvs");
     var ctx = canvas.getContext("2d"); 
     var reader = new FileReader();
-    var img = document.getElementById("preview");
+    var img = new Image();
 
     reader.addEventListener("load", () => {
         img.src = reader.result;
@@ -21,11 +21,17 @@ function processImg(){
     img.onload = () => {
         document.getElementById('filename').innerText = uploader.name;
 
-        ctx.drawImage(img, 0, 0);
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+        console.log("Image drawing complete.");
+        
         pixels = ctx.getImageData(0, 0, img.width, img.height);
 
-        img.style.height = '256px';
-        img.style.width = 'auto';
+        // img.style.height = '256px';
+        // img.style.width = 'auto';
 
         let lines = [];
 
@@ -34,42 +40,107 @@ function processImg(){
 
         console.log(pixels.data.length);
 
-        for(let x = 0; x < pixels.width; x++){ // TODO change
-            for(let y = 0; y < pixels.height; y++){
-                let hasVert = false;
-                let hasHoriz = false;
+        console.log(pixels.data);
 
 
+        // horizontal lines
+
+        for(let y = 0; y < pixels.height; y++){
+            let hasLine = false;
+            let lineStart = -1;
+            for(let x = 0; x < pixels.width; x++){
+                let pixLine = false;
                 for(let k = 0; k < 4; k++){
-                    if(Math.abs(getPixel(x, y, k) - getPixel(x+1, y, k)) > thresh) hasVert = true;
-                    if(Math.abs(getPixel(x, y, k) - getPixel(x, y+1, k)) > thresh) hasHoriz = true;
+                    if(Math.abs(getPixel(x, y, k) - getPixel(x, y+1, k)) >= thresh) {
+                        pixLine = true;
+                        break;
+                    }
                 }
 
-                let newX = x - pixels.width/2;
-                let newY = -y + pixels.height/2;
+                if(pixLine){
+                    if(!hasLine){
+                        hasLine = true;
+                        lineStart = getX(x);
+                    }
+                } else {
+                    if(hasLine){
+                        lines.push({
+                            dir: 1,
+                            offset: getY(y) - 1,
+                            start: lineStart,
+                            end: getX(x)
+                        });
+                        if(lines.length >= 5000){
+                            document.getElementById('filename').innerText = "Image too complex!";
+                            return;
+                        }
 
-                if(hasVert){
-                    lines.push({
-                        dir: 0,
-                        offset: newX+1,
-                        start: newY-1,
-                        end: newY
-                    });
+                        hasLine = false;
+                    }
                 }
+            }
 
-                if(hasHoriz){
-                    lines.push({
-                        dir: 1,
-                        offset: newY-1,
-                        start: newX,
-                        end: newX+1
-                    });
-                }
+            if (hasLine) {
+                lines.push({
+                    dir: 1,
+                    offset: getY(y) - 1,
+                    start: lineStart,
+                    end: getX(pixels.width)
+                });
 
-                
-
+                hasLine = false;
             }
         }
+
+        // vertical lines
+        for(let x = 0; x < pixels.width; x++){
+            let hasLine = false;
+            let lineStart = -1;
+            for(let y = pixels.height - 1; y >= 0; y--){
+                let pixLine = false;
+                for(let k = 0; k < 4; k++){
+                    if(Math.abs(getPixel(x, y, k) - getPixel(x+1, y, k)) >= thresh) {
+                        pixLine = true;
+                        break;
+                    }
+                }
+
+                if(pixLine){
+                    if(!hasLine){
+                        hasLine = true;
+                        lineStart = getY(y);
+                    }
+                } else {
+                    if(hasLine){
+                        lines.push({
+                            dir: 0,
+                            offset: getX(x) + 1,
+                            start: lineStart-1,
+                            end: getY(y)-1
+                        });
+
+                        if(lines.length >= 5000){
+
+                            document.getElementById('filename').innerText = "Image too complex!";
+                            return;
+                        }
+
+                        hasLine = false;
+                    }
+                }
+            }
+            if (hasLine) {
+                lines.push({
+                    dir: 0,
+                    offset: getX(x) + 1,
+                    start: lineStart-1,
+                    end: getY(pixels.height)-1
+                });
+
+                hasLine = false;
+            }
+        }
+
 
         console.log("DONE! Created " + lines.length + " lines.");
         let output = document.querySelector("#output");
@@ -77,7 +148,7 @@ function processImg(){
         output.innerHTML = "";
 
         lines.forEach((line) => {
-            output.innerHTML += getPrint(line);
+            output.innerHTML += getLineEquation(line);
         });
 
         output.select();
@@ -88,7 +159,7 @@ function processImg(){
     };
 }
 
-function getPrint(line){
+function getLineEquation(line){
     if(line.dir == 0){
 
         return "x = " + line.offset + "\\left\\{" + line.start + "\\le y \\le" + line.end + "\\right\\}\n";
@@ -101,5 +172,9 @@ function getPrint(line){
 } 
 
 function getPixel(i, j, c){
+    
     return pixels.data[j * 4 * pixels.width + 4 * i + c];
 }
+
+function getX(x) { return x - pixels.width/2; }
+function getY(y) { return -y + pixels.height/2; }
